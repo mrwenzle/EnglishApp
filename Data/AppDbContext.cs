a5f9cfe65735754ff500317f1a99e8a1d72fe3a1
Gabriel_Santos <mrwenzle10@gmail.com>
2026-01-03 01:05:18 -0300

    Adicionar arquivos de projeto.

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using EnglishApp.Models;
+using Microsoft.EntityFrameworkCore;
+using EnglishApp.Models;
+using System;
+using System.IO;
+
+namespace EnglishApp.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<WordCard> WordCards { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var folder = Environment.SpecialFolder.LocalApplicationData;
+            var path = Environment.GetFolderPath(folder);
+            var dbPath = Path.Join(path, "myenglishpal.db");
+
+            optionsBuilder.UseSqlite($"Data Source={dbPath}");
+        }
+    }
+}
---
+++ Models/WordCard.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace EnglishApp.Models
+{
+    public class WordCard
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Word { get; set; } = string.Empty;
+        public string Phonetic { get; set; } = string.Empty;
+        public string Definition { get; set; } = string.Empty;
+        public string ExampleSentence { get; set; } = string.Empty;
+        public string AudioUrl { get; set; } = string.Empty; 
+
+        public DateTime NextReviewDate { get; set; } = DateTime.Now; 
+        public int ReviewStep { get; set; } = 0;
+        public DateTime DateAdded { get; set; } = DateTime.Now;
+    }
+}
